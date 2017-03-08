89c2aed83c4136cff8737fbc6a911c7f3d6c173b
Enether <familyguyuser192@windowslive.com>
2017-03-08 14:57:59 +0200

    Added 2nd problem - real numbers

---
+++ cpp-basic-syntax/real-number-sign/real-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+    Write a program that shows the sign (+ or -) of the product of
+    three real numbers without calculating it. Use a sequence of if
+    statements.
+    */
+    double a, b, c;
+    cin >> a >> b >> c;
+    int negativeNumbersCount = (int)(a < 0) + (int)(b < 0) + (int)(c < 0);
+    
+    if (negativeNumbersCount % 2 == 0) {
+        cout << "+";
+    } else {
+        cout << "-";
+    }
+    return 0;
+}
